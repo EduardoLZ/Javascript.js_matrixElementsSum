59ee88a788bf6f461b836181a67546eff46cab03
EduardoLZ <ejlz95@hotmail.com>
2022-06-12 00:28:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+function solution(matrix) {
 
+    var x;
+    var y;
+    var s=0;
+    for(x=0;x<matrix.length;x++)
+        {
+           for(y=0;y<matrix[[x]].length;y++)
+            {
+                
+                if(x>0)
+                    {
+                        if(matrix[x-1][y]==0)
+                            {
+                                 matrix[x][y]=0;  
+                            }
+                        else
+                        {
+                            s=s+matrix[x][y];
+                        }
+                    }
+                else 
+                    {
+                        s=s+matrix[x][y];
+                    }
+            } 
+        }
+    
+    
+    return s;
+}
